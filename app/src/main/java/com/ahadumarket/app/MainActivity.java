0107b2711995a479df8e61ab137fab07ec459590
Yohannis Aweke <99585572+jooon2994@users.noreply.github.com>
2026-05-03 13:25:41 +0300

    Update MainActivity.java

--- app/src/main/java/com/ahadumarket/app/MainActivity.java
+++ app/src/main/java/com/ahadumarket/app/MainActivity.java
@@ -22,7 +22,7 @@ import android.webkit.WebViewClient;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
-import android.widget.SwipeRefreshLayout;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
